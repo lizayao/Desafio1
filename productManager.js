bd063c872c2005b9a9838992fd32d88bbe540ce7
lizayao <lizayao95@hotmail.com>
2023-06-13 19:39:45 -0300

    d

---
+++ productManager.js
@@ -0,0 +1,61 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(data) {
+        const newProduct = {
+            id: this.products.length + 1,
+            title: data.title,
+            description: data.description,
+            price: data.price,
+            thumbnail: data.thumbnail,
+            code: data.code,
+            stock: data.stock,
+        }
+
+        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+        const missingFields = requiredFields.filter(field => !data[field]);
+        if (missingFields.length > 0) {
+            console.error('Error, faltan completar los siguientes campos: ', missingFields);
+            return;
+        }
+
+        const codeExists = this.products.some(product => product.code === data.code);
+
+        if (codeExists) {
+            console.error("El código del producto ya existe:", data.code);
+            return;
+        }
+        this.products.push(newProduct);
+    }
+
+    getProducts(){
+        return this.products;
+    }
+
+    getProductById(id){
+        const product = this.products.find((p) => p.id === id);
+        if(product){
+            return product;
+        }else{
+            console.log("Not found");
+            return null;
+        }
+    }
+}
+
+const manager = new ProductManager();
+
+const product = {
+    title: 'Taza de gato',
+    description: 'Taza de porcelana con forma de gato',
+    price: 2000,
+    thumbnail: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/369/984/products/taza-lila____1-08de3701ce228fd8a416234187100692-640-0.jpg',
+    code: 'PROD563',
+    stock: 50
+};
+
+manager.addProduct(product);
+
+console.log(manager.getProducts())
